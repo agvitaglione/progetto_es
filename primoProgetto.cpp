e073586c921de3f026a30f6742bc797e442c8531
agvitaglione <77239833+agvitaglione@users.noreply.github.com>
2022-06-28 12:52:54 +0200

    new update

--- primoProgetto.cpp
+++ primoProgetto.cpp
@@ -1,34 +1,34 @@
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
+#include <slope/slope.h>
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
 
-// SWIPE ACT
+
+/* SWIPE ACT
+* GESTURE SPEED LIMIT FOR HORIZONTAL SWIPE RECOGNITION
+*/
 #define XACT 500
 #define YLIMIT 200
 
 // MACRO
-#define SWIPE_RIGHT(v_x, v_y) (v_x >= XACT && abs(v_y) < YLIMIT)
-#define SWIPE_LEFT(v_x, v_y) (v_x <= -XACT && abs(v_y) < YLIMIT)
+#define SWIPE_RIGHT(v_x, v_y) (v_x >= XACT && abs(v_y) < YLIMIT) // CHECK IT'S A RIGHT SWIPE
+#define SWIPE_LEFT(v_x, v_y) (v_x <= -XACT && abs(v_y) < YLIMIT) // CHECK IT'S A LEFT SWIPE
 
-
+// GLOBAL WIDGET INTERFACE VARIABLES
 GtkBuilder *builder;
 GtkWidget *window;
 GtkWidget *stack;
 GtkWidget *togglebottomleft;
 GtkWidget *togglebottomright;
+GtkWidget *box;
+SlopeFigure *figure;
+GtkWidget *view;
+SlopeScale *scale;
+SlopeItem *series;
 
-
-extern "C" G_MODULE_EXPORT void on_button1_clicked (GtkButton *button1, GtkStack *stack) {
-	//gtk_main_quit();
-	const gchar *name = gtk_widget_get_name(gtk_stack_get_visible_child(stack));
-	if(strcmp(name, "pagina2") == 0)
-		gtk_stack_set_visible_child_name(stack, "pagina1");
-	else
-		gtk_stack_set_visible_child_name(stack, "pagina2");
-}
-
+// SWIPE CALLBACK
 void change_page(GtkGestureSwipe *swipe, gdouble v_x, gdouble v_y) {
 	
 	GtkStack* stack = GTK_STACK(::stack);
@@ -53,6 +53,37 @@ void change_page(GtkGestureSwipe *swipe, gdouble v_x, gdouble v_y) {
 		}
 	}
 }
+#include <iostream>
+
+// CHART PLOT
+void chart_plot() {
+	figure = slope_figure_new();
+	scale = slope_xyscale_new();
+	view   = slope_view_new();
+	gtk_box_pack_start(GTK_BOX(box), GTK_WIDGET(view), TRUE, TRUE, 0);
+	slope_view_set_figure(SLOPE_VIEW(view), figure);
+	slope_figure_add_scale(SLOPE_FIGURE(figure), scale);
+	
+
+	const long n = 100;
+	double *x, *y;
+
+	x = (double*) g_malloc(n * sizeof(double));
+	y = (double*) g_malloc(n * sizeof(double));
+
+	for(long i = 0; i < 100; i++) {
+		x[i] = g_random_double();
+		y[i] = g_random_double();
+	}
+	slope_view_redraw(SLOPE_VIEW(view));
+	series = slope_xyseries_new_filled("Identical", x, y, 100, "kor");
+	slope_scale_add_item(scale, series);
+
+	// DELETE SIGNAL HANDLER MOUSE MOVING
+	g_signal_handlers_disconnect_by_data(view, GINT_TO_POINTER(SLOPE_MOUSE_MOVE));
+
+}
+
 
 int main(int argc, char* argv[]) {
 	
@@ -64,22 +95,28 @@ int main(int argc, char* argv[]) {
 	stack = GTK_WIDGET(gtk_builder_get_object(builder, "stack"));
 	togglebottomleft = GTK_WIDGET(gtk_builder_get_object(builder, "circle_page_1"));
 	togglebottomright = GTK_WIDGET(gtk_builder_get_object(builder, "circle_page_2"));
-	
-	// GESTURE
+	box = GTK_WIDGET(gtk_builder_get_object(builder, "pagina2"));
+	// ADD GESTURE LISTENER
 	GtkGesture *swipe = gtk_gesture_swipe_new(window);
 	g_signal_connect(swipe, "swipe", G_CALLBACK(change_page), NULL);
 	
-	// CHIUSURA
+	// ADD CHART
+	chart_plot();
+
+	// ADD DESTROY SIGNAL AT WINDOW CLOSING
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	gtk_builder_connect_signals(builder, NULL);
 
-	// FINESTRA
+
+
+	// SHOW MAIN WINDOW
 	gtk_widget_show_all(window);
 
 
-	// CURSORE
+	// CURSOR (TO DELETE ON TOUCH SCREEN) 
 	gdk_window_set_cursor(gtk_widget_get_window(window), gdk_cursor_new(GDK_ARROW));
 
+	// START GTK MAIN ROUTINE
 	gtk_main();
 
 	return 0;
