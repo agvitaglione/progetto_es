04f20de9f745ab3d338f16de93400b91ae687a02
marvitaglione <mariovitaglione@outlook.it>
2022-07-11 20:29:33 +0200

    update doc

--- include/RemoteConnection.h
+++ include/RemoteConnection.h
@@ -14,11 +14,39 @@ class RemoteConnection {
 
     public:
 
+        /**
+         * @brief Construct a new Remote Connection object
+         * 
+         */
         RemoteConnection();
+
+        /**
+         * @brief Destroy the Remote Connection object
+         * 
+         */
         ~RemoteConnection();
 
+        /**
+         * @brief Set the connection to a remote host. 
+         * 
+         * @param IP 
+         * @param port 
+         */
         void setConnection(std::string IP, uint16_t port);
+
+        /**
+         * @brief Send a message to a remote host. Should be called after setConnection.
+         * 
+         * @param message 
+         */
         void write(std::string message);
+
+        /**
+         * @brief It is usually called in case you are not sure whether connetion has been set up beforehand.
+         * 
+         * @return true 
+         * @return false 
+         */
         bool isConnected() const;
 
     private:
